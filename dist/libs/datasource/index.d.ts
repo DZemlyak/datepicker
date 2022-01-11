ba2515cc25640dd15a025e5f026e51d126551928
Dmitriy Zemlyak <dmitriy.zemlyak@hotmail.com>
2022-01-11 19:14:03 +0200

    Leave only compiled datepicker

--- dist/libs/datasource/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/public_api';
-//# sourceMappingURL=index.d.ts.map
--- dist/libs/datasource/matheo-datasource.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@matheo/datasource" />
-export * from './index';
-//# sourceMappingURL=matheo-datasource.d.ts.map
